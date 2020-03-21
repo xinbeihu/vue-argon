3c2d99df4e3d04753f04584f6fcf618f68efdd7c
Jia Ting <tanjt96@hotmail.com>
2020-03-21 15:14:39 +0800

    Edited Projects
    Projects List in "Page Content"

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,5 @@
 //Step 2 -- Define Routes
 import PageContent from './components/PageContent.vue'
-import AddItem from './components/AddItem.vue'
-export default[
-    { path: '/', component: PageContent},
-    { path: '/add', component: AddItem}
+export default [
+    { path: '/', component: PageContent }
 ]
