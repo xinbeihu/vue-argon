bac64af6a11bd82f92e774274c2848d209fbbf1c
Xinbei Hu <tkgs.hu.xinbei@gmail.com>
2020-03-24 22:52:36 +0800

    recover the files

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase';
+
 const firebaseConfig = {
     apiKey: "AIzaSyAJzuGdri_OLOX7hTZAdIUTzwceDVC3XWI",
     authDomain: "bt3103-final-product.firebaseapp.com",
@@ -6,9 +7,10 @@ const firebaseConfig = {
     projectId: "bt3103-final-product",
     storageBucket: "bt3103-final-product.appspot.com",
     messagingSenderId: "619318861214",
-    appId: "1:619318861214:web:b5e764801058894b91ca8f",
-    measurementId: "G-HB0D3FX5LV"
+    appId: "1:619318861214:web:ffa401fe7338cd7f91ca8f",
+    measurementId: "G-26YP3W344V"
 };
+
 firebase.initializeApp(firebaseConfig);
 var database = firebase.firestore();
 export default database;
--- src/routes.js
+++
@@ -1,5 +0,0 @@
-//Step 2 -- Define Routes
-import PageContent from './components/PageContent.vue'
-export default [
-    { path: '/', component: PageContent }
-]
