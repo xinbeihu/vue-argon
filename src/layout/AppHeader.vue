4ac628acc3a302bc64f350070ba3c05a6fc7a081
unknown <vcjw97@gmail.com>
2020-04-09 16:00:39 +0800

    Update AppHeader.vue

--- src/layout/AppHeader.vue
+++ src/layout/AppHeader.vue
@@ -1,20 +1,6 @@
 <template>
     <header class="header-global">
         <base-nav class="navbar-main" transparent type="" effect="light" expand>
-            <router-link slot="brand" class="navbar-brand mr-lg-5" to="/">
-                <img src="img/brand/white.png" alt="logo">
-            </router-link>
-
-            <div class="row" slot="content-header" slot-scope="{closeMenu}">
-                <div class="col-6 collapse-brand">
-                    <a href="https://demos.creative-tim.com/vue-argon-design-system/documentation/">
-                        <img src="img/brand/blue.png">
-                    </a>
-                </div>
-                <div class="col-6 collapse-close">
-                    <close-button @click="closeMenu"></close-button>
-                </div>
-            </div>
 
             <ul class="navbar-nav navbar-nav-hover align-items-lg-center">
                 <router-link to="/profile" class="dropdown-item">Profile</router-link>
