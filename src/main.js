d93ec7b98af83ca7c92b695fb59b27156bc24012
0mission <61975740+0mission@users.noreply.github.com>
2020-04-16 15:19:52 +0800

    Dashboard Added
    Added Dashboard

--- src/main.js
+++ src/main.js
@@ -22,6 +22,17 @@ import Argon from "./plugins/argon-kit";
 import './registerServiceWorker';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 
+// Register Charts globally
+import BarChart from './views/BarChart.vue'
+import LineChart from './views/LineChart.vue'
+import PieChart from './views/PieChart.vue'
+import Doughnut from './views/Doughnut.vue'
+
+Vue.component("BarChart", BarChart)
+Vue.component("LineChart", LineChart)
+Vue.component("PieChart", PieChart)
+Vue.component("Doughnut", Doughnut)
+
 
 Vue.config.productionTip = false;
 Vue.use(Argon);
