ab159ab9ec7ca7af463ba3ff2525f270818d5807
0mission <61975740+0mission@users.noreply.github.com>
2020-04-16 18:49:57 +0800

    Modified Doughnut Chart
    Multiple Doughnut Chart to display each module

--- src/views/Doughnut.js
+++ src/views/Doughnut.js
@@ -11,17 +11,19 @@ export default{
             datacollection: {
                 labels: [],
                 datasets: [
-                    {
-                    label: "Number of Tasks Completed",
-                    backgroundColor: ["#DAECFC", "#DDF3F2", "#F4F5F5","#EBE0FF", "#FFF3DA", "#FFE1E6"],
-                    borderColor: ["#36A2EB", "#4BC0C0", "#C9CBCF", "#9966FE", "#FFC134", "#FF6383"],
-                    // light ["#F4F5F5","#EBE0FF", "#DAECFC", "#DDF3F2", "#FFF3DA", "#FFE1E6"]
-                    // dark ["#C9CBCF", "#9966FE", "#36A2EB", "#4BC0C0", "#FFC134", "#FF6383"]
-                    data: []
-                }
+                //     {
+                //     label: "Number of Tasks Completed",
+                //     backgroundColor: ["#DAECFC", "#DDF3F2", "#F4F5F5","#EBE0FF", "#FFF3DA", "#FFE1E6"],
+                //     borderColor: ["#36A2EB", "#4BC0C0", "#C9CBCF", "#9966FE", "#FFC134", "#FF6383"],
+                //     // light ["#F4F5F5","#EBE0FF", "#DAECFC", "#DDF3F2", "#FFF3DA", "#FFE1E6"]
+                //     // dark ["#C9CBCF", "#9966FE", "#36A2EB", "#4BC0C0", "#FFC134", "#FF6383"]
+                //     data: [],
+                    
+                // }
             ]
             },
             options: {
+                cutoutPercentage: 50,
                 // responsive: true,
                 // maintainAspectRatio: false
             }
@@ -30,20 +32,20 @@ export default{
     methods: {
 
         fetchTasks: function() {
-            var user = firebase.auth().currentUser;
-            var emailVerified = user.email;
-            database.collection("User Info").onSnapshot(user => {
-                user.forEach(function(currUser) {
-                    if (currUser.id == emailVerified) {
-                        tempName = currUser.data()["Name"];
-                    }
-                });
-                this.user = tempName;
-            });
+            // get current user
+            // var user = firebase.auth().currentUser;
+            // var emailVerified = user.email;
+            // database.collection("User Info").onSnapshot(user => {
+            //     user.forEach(function(currUser) {
+            //         if (currUser.id == emailVerified) {
+            //             tempName = currUser.data()["Name"];
+            //         }
+            //     });
+            //     this.user = tempName;
+            // });
           let currUser = this.user;
           let tasks = {};
           let myGroups = { members: {}, groupName: {} };
-          let membercontribution = {};
     
           database.collection("Modules").onSnapshot(myModules => {
             myModules.forEach(function(module) {
@@ -62,13 +64,19 @@ export default{
             this.taskList = tasks;
             this.groups = myGroups;
             // console.log(myGroups)
+
+            for (module in myGroups["member"]) {
+                console.log(module)
+            };
     
-            // let selectedModule = "";
+            let selectedModule = "";
             let member = "";
             let num = "";
+            let length = 0;
             for (let module in tasks) {
-                // selectedModule = module;
-                // let taskArray = [];
+                selectedModule = module;
+                let taskArray = [];
+                let membercontribution = {};
                 for (let task in tasks[module]){
                     for (let assignment in tasks[module][task]) {
                         if (assignment == "subTasks") {
@@ -84,15 +92,15 @@ export default{
                         }
                     }
                 }
-                // for (member in membercontribution) {
-                //     this.datacollection.labels.push(member);
-                //     taskArray.push(membercontribution[member]);
-                // }
-                // this.datacollection.datasets.push({data:taskArray, label:selectedModule, backgroundColor: ["#FF6383", "#FF9F40", "#FFC134", "#4BC0C0", "#36A2EB"], borderWidth: 0.5});
-            }
-            for (member in membercontribution){
-                this.datacollection.labels.push(member);
-                this.datacollection.datasets[0].data.push(membercontribution[member]);
+                for (member in membercontribution) {
+                    this.datacollection.labels.push(member);
+                }
+                for (var i=length; i<(Object.keys(membercontribution).length + length); i++) {
+                    taskArray[i] = membercontribution[member];
+                }
+                length = length + taskArray.length;
+                
+                this.datacollection.datasets.push({data:taskArray, label:selectedModule, backgroundColor: ["#DAECFC", "#DDF3F2", "#F4F5F5","#EBE0FF", "#FFF3DA", "#FFE1E6"]});
             }
             this.renderChart(this.datacollection, this.options)
             });
