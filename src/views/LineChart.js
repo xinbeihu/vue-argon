47a0bd688a02687fe60744146e1b6d86ec6d543a
0mission <61975740+0mission@users.noreply.github.com>
2020-04-21 21:05:57 +0800

    Working Dashboard
    Working Dashboard with currUser

--- src/views/LineChart.js
+++ src/views/LineChart.js
@@ -34,38 +34,44 @@ export default {
     },
 
     fetchTasks: function () {
-      // get current user
-      var user1 = firebase.auth().currentUser;
-      var emailVerified = user1.email;
-      let tempName = "";
-      database.collection("User Info").onSnapshot(user2 => {
-        user2.forEach(function (currUser) {
+      var user = firebase.auth().currentUser;
+      var emailVerified = user.email;
+      var tempName = "";
+      let time = {};
+
+      database.collection("User Info").onSnapshot(users => {
+        users.forEach(function (currUser) {
           if (currUser.id == emailVerified) {
             tempName = currUser.data()["Name"];
           }
         });
         let currUser = tempName;
-        // console.log("currUser for LineChart here")
-        // console.log(currUser)
-
-        let tasks = {};
-        let myGroups = { members: {}, groupName: {} };
-        let time = {};
 
         database.collection("Modules").onSnapshot(myModules => {
+          let tasks = {};
+          let myGroups = { members: {}, groupName: {} };
           myModules.forEach(function (module) {
-            for (let groups in module.data()) {
+            console.log(Object.keys(module.data()));
+            for (let groups of Object.keys(module.data())) {
               if (groups == "NoGroup") {
-                return;
+                //groups = group name
+                continue;
               }
+
               if (!module.data()[groups]["Group Members"].includes(currUser)) {
-                return;
+                //filter to show only user's groups
+                continue;
               }
-              myGroups["members"][module.id] = module.data()[groups]["Group Members"];
+
+              myGroups["members"][module.id] = module.data()[groups][
+                "Group Members"
+              ];
               myGroups["groupName"][module.id] = groups;
+
               tasks[module.id] = module.data()[groups]["Assignments"];
             }
           });
+
           this.taskList = tasks;
           this.groups = myGroups;
 
@@ -95,6 +101,69 @@ export default {
         });
       });
     }
+
+    // fetchTasks: function () {
+    //   // get current user
+    //   var user1 = firebase.auth().currentUser;
+    //   var emailVerified = user1.email;
+    //   let tempName = "";
+    //   database.collection("User Info").onSnapshot(user2 => {
+    //     user2.forEach(function (currUser) {
+    //       if (currUser.id == emailVerified) {
+    //         tempName = currUser.data()["Name"];
+    //       }
+    //     });
+    //     let currUser = tempName;
+    //     // console.log("currUser for LineChart here")
+    //     // console.log(currUser)
+
+    //     let tasks = {};
+    //     let myGroups = { members: {}, groupName: {} };
+    //     let time = {};
+
+    //     database.collection("Modules").onSnapshot(myModules => {
+    //       myModules.forEach(function (module) {
+    //         for (let groups in module.data()) {
+    //           if (groups == "NoGroup") {
+    //             return;
+    //           }
+    //           if (!module.data()[groups]["Group Members"].includes(currUser)) {
+    //             return;
+    //           }
+    //           myGroups["members"][module.id] = module.data()[groups]["Group Members"];
+    //           myGroups["groupName"][module.id] = groups;
+    //           tasks[module.id] = module.data()[groups]["Assignments"];
+    //         }
+    //       });
+    //       this.taskList = tasks;
+    //       this.groups = myGroups;
+
+    //       let selectedModule = "";
+    //       let month = "";
+    //       let num = "";
+    //       for (let module in tasks) {
+    //         selectedModule = module;
+    //         let taskArray = [];
+    //         for (let task in tasks[selectedModule]) {
+    //           month = tasks[selectedModule][task]["deadline"].toDate().getMonth();
+    //           if (tasks[selectedModule][task]["completed"] == true) {
+    //             if (month in time) {
+    //               num = time[month];
+    //               time[month] = num + 1;
+    //             } else {
+    //               time[month] = 1;
+    //             }
+    //           }
+    //         }
+    //         for (month in time) {
+    //           taskArray[month] = time[month];
+    //         }
+    //         this.datacollection.datasets.push({ data: taskArray, label: selectedModule, borderColor: this.getColor(), fill: false })
+    //       }
+    //       this.renderChart(this.datacollection, this.options)
+    //     });
+    //   });
+    // }
   },
   created() {
     this.fetchTasks()
